3ac32f29e030d180e0de725978c078679c2885c5
sirojiddin3334 <toshpolatovaniluar17@gmail.com>
2026-08-20 11:03:54 +0500

    Add: 6) Boshlardagi va ohirdagi bo‘shliqni olib tashlash [sirojiddin3334]

---
+++ lms_kod_topshiriqlari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/06-6__Boshlardagi_va_ohirdagi_bo_shliqni_olib_tashlas.py
@@ -0,0 +1,5 @@
+text = input()
+print(text.strip())
+# Bo'shliqlari bo'lishi mumkin bo'lgan satr kiriting.
+# strip() yordamida boshlanish va oxiridagi bo'shliqlarni olib tashlang.
+# Natijani aynan o'zi bilan chiqaring.
