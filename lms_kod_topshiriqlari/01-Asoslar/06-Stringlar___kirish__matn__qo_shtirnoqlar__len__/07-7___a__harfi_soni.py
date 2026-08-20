aef79fb18e386a464232e39f5e9f5dd2f539985d
sirojiddin3334 <toshpolatovaniluar17@gmail.com>
2026-08-20 11:07:17 +0500

    Add: 7) 'a' harfi soni [sirojiddin3334]

---
+++ lms_kod_topshiriqlari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/07-7___a__harfi_soni.py
@@ -0,0 +1,6 @@
+text = input()
+count_a = text.count('a')
+print(f"a: {count_a}")
+# Bitta satr kiriting.
+# Undagi 'a' harfi sonini count('a') bilan hisoblang.
+# "a: <soni>" ko'rinishida chiqaring.
