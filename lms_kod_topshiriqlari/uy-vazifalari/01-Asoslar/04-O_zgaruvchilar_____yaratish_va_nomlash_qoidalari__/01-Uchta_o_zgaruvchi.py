31088cc341cfa9a614989032aada5be613657d48
sirojiddin3334 <toshpolatovaniluar17@gmail.com>
2026-09-11 00:22:40 +0500

    Add: Uchta o'zgaruvchi [sirojiddin3334]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Uchta_o_zgaruvchi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Uchta_o_zgaruvchi.py
@@ -1,3 +1,7 @@
-print("Ism: Aziz")
-print("Kurs: Python")
-print("Modul: Asoslar")
+ism = "Aziz"
+kurs = "Python"
+modul = "Asoslar"
+
+print("Ism:", ism)
+print("Kurs:", kurs)
+print("Modul:", modul)
