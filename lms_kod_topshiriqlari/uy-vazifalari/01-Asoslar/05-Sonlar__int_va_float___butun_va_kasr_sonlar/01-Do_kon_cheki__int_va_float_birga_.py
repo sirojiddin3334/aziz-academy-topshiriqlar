0037a87f8d58f8a18396f014e6e5a8bc8836ff04
sirojiddin3334 <toshpolatovaniluar17@gmail.com>
2026-09-11 21:39:38 +0500

    Add: Do'kon cheki (int va float birga) [sirojiddin3334]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/01-Do_kon_cheki__int_va_float_birga_.py
@@ -1,4 +1,9 @@
+mahsulot = "Non"
+narx1 = 4500
+mahsulot2 = "Sut"
+narx2 = 12.5
+
 print("=== CHEK ===")
-print("Non - 4500")
-print("Sut - 12.5")
+print(mahsulot, "-", narx1)
+print(mahsulot2, "-", narx2)
 print("Rahmat!")
